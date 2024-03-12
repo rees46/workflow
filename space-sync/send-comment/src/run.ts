f9176ec090a356b8d85f7d0ffd87324db440693a
Nikita Elfimov <nelfimov@mail.ru>
2024-03-12 13:26:15 +0300

    feat: build action, log event

--- space-sync/send-comment/src/run.ts
+++ space-sync/send-comment/src/run.ts
@@ -3,26 +3,22 @@ import { setFailed }    from '@actions/core'
 import { context }      from '@actions/github'
 import { getClient }    from '@space/client'
 import { errorHandler } from '@space/error-handler'
-import { findTicketID } from './find-ticket-id'
-import { getIssueBody } from './get-issue-body'
+import { findTicketID } from './parsers'
+import { getIssueBody } from './parsers'
 
 export const run = () => {
   try {
     const issueBody = getIssueBody(context)
 
-    console.log(issueBody)
+    console.log(context)
 
     const issueTitleWithTicketID = getInput('blockWithTicketID', { required: true })
-    const projectID = getInput('projectID', { required: true })
 
     const spaceIssueId = findTicketID(issueBody, issueTitleWithTicketID)
 
     const client = getClient({ basePath: process.env.SPACE_URL, apiKey: process.env.SPACE_SECRET })
 
     // client.projectsProjectPlanningIssuesIssueIdCommitsPost(projectID, spaceIssueId, {})
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(context.payload, undefined, 2)
   } catch (error) {
     errorHandler(error, setFailed)
   }
