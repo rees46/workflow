bbb0c98790f70fdd8d7114dabed18af4e8a5c2c8
Nikita Elfimov <nelfimov@mail.ru>
2024-03-12 18:17:52 +0300

    fix: error handler

--- space-sync/error-handler/src/error.handler.ts
+++ space-sync/error-handler/src/error.handler.ts
@@ -1,8 +1,16 @@
+import { AxiosError } from 'axios'
+
 type Callback = (value: string | Error) => unknown
 
 type ErrorHandler = (error: unknown, callback: Callback) => void
 
 export const errorHandler: ErrorHandler = (error, callback) => {
+  if (error instanceof AxiosError) {
+    callback(error.message)
+    error.status && callback(error.status)
+    callback(JSON.stringify(error.response?.data, undefined, 2))
+  }
+
   if (error instanceof Error || typeof error === 'string') {
     callback(error)
   }
