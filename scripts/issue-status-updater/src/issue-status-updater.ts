6268dd70392155ac926c06b69b8b9d7297ab9371
Nikita Elfimov <nelfimov@mail.ru>
2024-07-30 13:01:58 +0300

    feat: scripts

---
+++ scripts/issue-status-updater/src/issue-status-updater.ts
@@ -0,0 +1,24 @@
+import { client } from '@global/github-client'
+
+type UpdateIssueStatus = (projectId: string, projectIssueId: string, fieldId: string, optionId: string) => Promise<void>
+
+export const updateIssueStatus: UpdateIssueStatus = async (projectId, issueId, fieldId, optionId) => {
+  const mutation = `#graphql
+    mutation($projectId: ID!, $issueId: ID!, $fieldId: ID!, $optionId: String!) {
+      updateProjectV2ItemFieldValue(input: {projectId: $projectId, itemId: $issueId, fieldId: $fieldId, value: {singleSelectOptionId: $optionId}}) {
+        projectV2Item {
+          id
+        }
+      }
+    }
+  `;
+
+  await client.graphql(mutation, {
+    issueId,
+    fieldId,
+    optionId,
+    projectId,
+  });
+
+  console.log(`Issue ${issueId} перемещён в статус`);
+}
---
+++ scripts/project-field-getter/src/project-field-getter.ts
@@ -0,0 +1,47 @@
+import { client } from '@global/github-client'
+
+type GetFieldAndOptionIds = (projectId: string, fieldName: string, optionName: string) => Promise<{ fieldId: string, optionId: string } | undefined>
+
+export const getFieldAndOptionIds: GetFieldAndOptionIds = async (
+  projectId,
+  fieldName = "Status",
+  optionName = "Postponed",
+) => {
+  const query = `
+    query($projectId: ID!) {
+      node(id: $projectId) {
+        ... on ProjectV2 {
+          fields(first: 20) {
+            nodes {
+              ... on ProjectV2SingleSelectField {
+                id
+                name
+                options {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const response = await client.graphql(query, {
+    projectId,
+  });
+
+  const fields = response.node.fields.nodes;
+  const field = fields.find((field) => field.name === fieldName);
+  if (!field) {
+    throw new Error(`Поле с именем ${fieldName} не найдено`);
+  }
+
+  const option = field.options.find((option) => option.name === optionName);
+  if (!option) {
+    throw new Error(`Опция с именем ${optionName} не найдена`);
+  }
+
+  return { fieldId: field.id, optionId: option.id };
+}
