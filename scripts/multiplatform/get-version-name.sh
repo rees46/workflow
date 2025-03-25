2739c7f1b11dd51a8fdd03e5f84ae412f7169c09
Kirill Ivanov <kirill-ivanovvvvvvv@yandex.ru>
2025-03-25 17:35:55 +0300

    refactor(github): useless (#269)
    * refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): change uses to p12 cert
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    feat(scripts): version-properties
    
    feat(scripts): version-properties
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    refactor(github): useless
    
    * fix(github): action version
    
    * refactor(scripts): directory naming
    
    * feat(scripts): intermediate logging

---
+++ scripts/multiplatform/get-version-name.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ ! -f version.properties ]; then
+  echo "version.properties not found"
+  exit 1
+fi
+
+version=$(grep "^VERSION_NAME=" version.properties | cut -d'=' -f2)
+echo "VERSION_CODE: $version">&2
+echo $version
