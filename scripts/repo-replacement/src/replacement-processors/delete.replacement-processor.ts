97a239ba0d271657b24d2f57d07dc6ae02376c30
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-22 19:06:17 +0300

    feat(scripts): delete replacement processor

---
+++ scripts/repo-replacement/src/replacement-processors/delete.replacement-processor.ts
@@ -0,0 +1,22 @@
+import fs from "node:fs";
+import path from "node:path";
+
+import type { DeleteReplacementProcessorType } from "../interfaces/index.js";
+
+export const deleteReplacementProcessor: DeleteReplacementProcessorType = ({
+  targetDir,
+  deletions,
+}) => {
+  if (deletions) {
+    deletions.forEach((deletePath) => {
+      const absoluteDeletePath = path.resolve(targetDir, deletePath);
+      console.log(absoluteDeletePath);
+      fs.rmSync(absoluteDeletePath, { recursive: true, force: true });
+      console.info(`Delete: ${deletePath}`);
+    });
+
+    return;
+  }
+
+  console.info(`Deletions is empty, skip`);
+};
