9b67105b063a7eb557cc68c8cd0a8c0dc47b3477
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-22 17:29:49 +0300

    refactor(scripts): separate main

--- scripts/repo-replacement/src/replacement-processors/index.ts
+++ scripts/repo-replacement/src/replacement-processors/index.ts
@@ -1,3 +1,4 @@
 export * from "./apply.replacement-processor.js";
 export * from "./directory.replacements-processor.js";
 export * from "./file-content.replacement-processor.js";
+export * from "./main.replacement-processor.js";
