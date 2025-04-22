5aa73bf775d46a32c3072994e5e1116f2c484fe1
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-22 17:30:07 +0300

    refactor(scripts): entrypoint

--- scripts/repo-replacement/src/entrypoint.ts
+++ scripts/repo-replacement/src/entrypoint.ts
@@ -1,41 +1,23 @@
 import type { Config } from "./interfaces/index.js";
+import { getArguments } from "./getters/index.js";
 
 import { checkDirectoryExist } from "./utils/index.js";
 import { direcotryReplacementProcessor } from "./replacement-processors/index.js";
 import { applyReplacementProcessor } from "./replacement-processors/index.js";
 import { fileContentReplacementProcessor } from "./replacement-processors/index.js";
 import { escapeRegExpUtil } from "./utils/index.js";
+import { checkArgumentsUtil } from "./utils/index.js";
+import { main } from "./replacement-processors/index.js";
 
-if (require.main === module) {
-  const [configString, targetDir] = process.argv.slice(2);
+const { configString, targetDir } = getArguments();
+checkArgumentsUtil({ configString, targetDir });
 
-  if (!configString || !targetDir) {
-    console.error(
-      "Usage: yarn process <config-json-string> <target-directory>",
-    );
-    process.exit(1);
-  }
-
-  main(configString, targetDir);
-}
-
-async function main(configString: string, targetDir: string) {
-  try {
-    const config: Config = JSON.parse(configString);
-
-    checkDirectoryExist(targetDir);
-
-    console.log("Starting replacement process...");
-    await direcotryReplacementProcessor({
-      directory: targetDir,
-      config,
-      applyReplacementProcessor,
-      fileContentReplacementProcessor,
-      escapeRegExpUtil,
-    });
-    console.log("Replacement completed successfully!");
-  } catch (error) {
-    console.error("Error:", error);
-    process.exit(1);
-  }
-}
+main({
+  configString,
+  targetDir,
+  checkDirectoryExist,
+  direcotryReplacementProcessor,
+  applyReplacementProcessor,
+  fileContentReplacementProcessor,
+  escapeRegExpUtil,
+});
