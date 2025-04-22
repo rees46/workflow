2e0dd1fc3dec99bfeebdd4696ac27726ff1bad52
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-22 17:29:57 +0300

    feat(scripts): checks

---
+++ scripts/repo-replacement/src/utils/check-arguments.util.ts
@@ -0,0 +1,12 @@
+import type { CheckArgumentsUtilType } from "../interfaces/index.js";
+
+export const checkArgumentsUtil: CheckArgumentsUtilType = ({
+  configString,
+  targetDir,
+}) => {
+  if (!configString || !targetDir) {
+    console.error("Not all arguments provided");
+    console.info("Usage: yarn process <config-json-string> <target-directory>");
+    process.exit(1);
+  }
+};
--- scripts/repo-replacement/src/utils/index.ts
+++ scripts/repo-replacement/src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./escape-reg-exp.util.js";
 export * from "./check-directory-exist.util.js";
+export * from "./check-arguments.util.js";
