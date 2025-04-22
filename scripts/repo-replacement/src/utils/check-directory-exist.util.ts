30d92448c88ef2812401339f15e0b5670d57a44a
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-22 17:29:57 +0300

    feat(scripts): checks

--- scripts/repo-replacement/src/utils/check-directory-exist.util.ts
+++ scripts/repo-replacement/src/utils/check-directory-exist.util.ts
@@ -1,6 +1,7 @@
 import fs from "node:fs";
+import type { CheckDirectoryExistType } from "../interfaces/index.js";
 
-export const checkDirectoryExist = (dirPath: string): void => {
+export const checkDirectoryExist: CheckDirectoryExistType = (dirPath) => {
   if (!fs.existsSync(dirPath)) {
     throw new Error(`Directory ${dirPath} does not exist`);
   }
