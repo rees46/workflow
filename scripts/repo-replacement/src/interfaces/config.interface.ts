46cbeb75507f4b838331c691bd50a0758eff00f6
Kirill Ivanov <iwwwanow.work@yandex.ru>
2025-04-25 14:21:56 +0300

    feat(scripts): ignore replacements

--- scripts/repo-replacement/src/interfaces/config.interface.ts
+++ scripts/repo-replacement/src/interfaces/config.interface.ts
@@ -3,4 +3,5 @@ import type { Replacement } from "./replacement.interface.js";
 export interface Config {
   replacements: Array<Replacement>;
   deletions: Array<string>;
+  ignore: Array<string>;
 }
